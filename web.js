541857a2fa9f07c3a407a78b39eea4b3d1dbad92
Kevin Lynch <klynch@gmail.com>
2013-07-11 18:57:07 -0400

    read file with correct encoding

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var f = fs.readFileSync('index.html', { encoding: 'UTF-8' });
+  var f = fs.readFileSync('index.html', { encoding: 'utf8' });
 
   response.send(f);
 });
